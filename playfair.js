a01fa786dd59b58310990ce3271f214aa4b0fb27
devprogramer <fesenko.victor@gmail.com>
2016-10-25 17:55:32 +0300

    Update playfair.js

--- playfair.js
+++ playfair.js
@@ -1,11 +1,58 @@
-for(var i=0;i<1297;i++){
-          var code = i.toString(16);
-          if(code.length<4){
-            while(code.length<4){
-              code = "0"+code;
-            }
+ var text = "ППриветик я тебя узнал",
+            stopSymbol = 0;
+        function fullFillMatrix(){
+          var symbolsMatrix = [];
+          var code = 0;
+          for (var i=0; i< 36; i++){
+              for(var j=0; j< 36; j++){
+                if(!symbolsMatrix[i]) symbolsMatrix[i] = [];
+                symbolsMatrix[i][j] = code;
+                code ++;
+                // console.log(symbolsMatrix[i][j]);
+              }
           }
-          console.log(String.fromCharCode(i));
+          return symbolsMatrix;
         }
 
-    
+        function findPosition(symb, symbolsMatrix){
+            var x = null,
+                y = null;
+
+
+          for (var i=0; i< 36; i++){
+              for(var j=0; j< 36; j++){
+                if( symbolsMatrix[i][j] == symb ){
+                  y = i;
+                  x=j;
+                }
+                
+                if(x !== null) break;
+              }
+              if(x !== null) break;
+          }
+
+          return {x: x, y: y};
+        }
+
+
+        function createBigrams(text){
+          var bigrams = [];
+          var tmp = [];
+          for(var i = 0; i<text.length; i++){
+              tmp.push(text.charCodeAt(i));
+
+              if(text.charCodeAt(i+1) && text.charCodeAt(i+1) == text.charCodeAt(i)){
+                tmp.push(stopSymbol);
+              }
+              if(tmp.length === 2){
+                bigrams.push(tmp);
+                tmp = [];
+              }
+          }
+          if(tmp.length === 1){
+            tmp.push(stopSymbol);
+            bigrams.push(tmp);
+
+          } 
+          return bigrams;
+        }
